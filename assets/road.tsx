5995f2e2f55dd1fc1894594a0a9532bfa64e37e1
cailinjefferson567 <cailin.jefferson.567@my.csun.edu>
2023-09-19 14:34:52 -0700

    fixed files kinda

--- assets/road.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="road" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="Dropbox/PC/Downloads/TX Tileset Stone Ground.png" width="256" height="256"/>
-</tileset>
