955e930e24232bdf08b62764dca3127130d0f335
Mohammed Ikhlaas Hussain <112817139+seamohammed@users.noreply.github.com>
2023-09-23 16:55:27 -0700

    Updated from extending IteratingSystem to EntitySystem

--- core/src/main/java/com/csun/game/ashley/systems/RenderSystem.java
+++ core/src/main/java/com/csun/game/ashley/systems/RenderSystem.java
@@ -2,38 +2,48 @@ package com.csun.game.ashley.systems;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
-import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Array;
 import com.csun.game.ashley.components.TextureComponent;
 import com.csun.game.ashley.components.TransformComponent;
 
-public class RenderSystem extends IteratingSystem {
-
-    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
+public class RenderSystem extends EntitySystem {
 
     private final SpriteBatch spriteBatch = new SpriteBatch();
+    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
+    private final Family family = Family.all(TextureComponent.class, TransformComponent.class).get();
+    private Array<Entity> entities;
 
-    public RenderSystem() {
-        super(Family.all(TextureComponent.class, TransformComponent.class).get());
+    @Override
+    public void addedToEngine(com.badlogic.ashley.core.Engine engine) {
+        entities = engine.getEntitiesFor(family);
     }
 
     @Override
-    public void processEntity(Entity entity, float deltaTime) {
-        TextureComponent texture;
-        TransformComponent transform;
-
-        texture = entity.getComponent(TextureComponent.class);
-        transform = entity.getComponent(TransformComponent.class);
-
+    public void update(float deltaTime) {
         spriteBatch.begin();
-        spriteBatch.draw(texture.textureRegion, 50, 50);
+
+        for (Entity entity : entities) {
+            TextureComponent texture = entity.getComponent(TextureComponent.class);
+            TransformComponent transform = entity.getComponent(TransformComponent.class);
+
+            spriteBatch.draw(texture.textureRegion, transform.vector2.x, transform.vector2.y);
+        }
+
         spriteBatch.end();
 
-      //  shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-      //  shapeRenderer.setColor(Color.CYAN);
-       // shapeRenderer.circle(transform.vector2.x, transform.vector2.y, 20);
-       // shapeRenderer.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        for (Entity entity : entities) {
+            TransformComponent transform = entity.getComponent(TransformComponent.class);
+
+            shapeRenderer.setColor(Color.CYAN);
+            shapeRenderer.circle(transform.vector2.x, transform.vector2.y, 20);
+        }
+
+        shapeRenderer.end();
     }
 }
+
