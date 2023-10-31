a462cebbc8f3c7ff326abaf0b051a4764a15fe5a
MNuila1 <135813125+MNuila1@users.noreply.github.com>
2023-10-30 19:25:03 -0700

    Update Particle.java

--- core/src/main/java/com/csun/game/weather/Particle.java
+++ core/src/main/java/com/csun/game/weather/Particle.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameOne.GameObject;
 
-public class Particle extends GameObject {
+public class Particle {
     public Particle(Vector2 pos, Texture img) {
         super(pos, img);
     }
