10e1a576a4a44858c4f83f7691158eb137661e47
John <55899462+JohnRsps77@users.noreply.github.com>
2023-11-20 19:49:14 -0800

    Removed explicit array creation for chat dialogue

--- core/src/main/java/com/csun/game/dialogue/ChatDialogue.java
+++ core/src/main/java/com/csun/game/dialogue/ChatDialogue.java
@@ -3,7 +3,7 @@ package com.csun.game.dialogue;
 public class ChatDialogue implements DialoguePart {
     private final String[] lines;
 
-    public ChatDialogue(String[] lines) {
+    public ChatDialogue(String... lines) {
         this.lines = lines;
     }
 }
--- core/src/main/java/com/csun/game/dialogue/DialogueTest.java
+++ core/src/main/java/com/csun/game/dialogue/DialogueTest.java
@@ -4,20 +4,18 @@ public class DialogueTest {
 
     public DialogueChain t() {
         return new DialogueChain.Builder(3)
-            .addChatDialogue(new ChatDialogue(new String[]{
+            .addChatDialogue(new ChatDialogue(
                 "This is npc talking line 1",
-                "This is line 2"
-            }))
+                "This is line 2"))
             .addOptionsDialogue(
                 new OptionsDialogue("Select an option",
                     new Option("Do something1", this::doingSomething),
                     new Option("Do something2", this::doingSomething),
                     new Option("Do something3", this::doingSomething))
             )
-            .addChatDialogue(new ChatDialogue(new String[]{
+            .addChatDialogue(new ChatDialogue(
                 "This is npc talking line 1",
-                "This is line 2"
-            }))
+                "This is line 2"))
             .build();
     }
 
